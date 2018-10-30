e2728c4c2d30fa5ae1889cf5a7d99d73f3350f24
Kate <ms.katesy@gmail.com>
2018-10-30 21:05:00 +1100

    video 17

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,10 +1,52 @@
 import React from 'react'
+import {formatPrice} from '../helpers'
 
 export default class Order extends React.Component{
 
+  renderOrder = (key) => {
+    const fish = this.props.fishes[key]
+    const count = this.props.order[key]
+    const isAvailable = fish.status === 'available'
+    if(!isAvailable) {
+      return <li key={key}> Sorry { fish ? fish.name : 'fish '} is no longer available</li>
+
+    } else { 
+      return <li key={key}>
+        {count} lbs {fish.name}
+        {formatPrice(count * fish.price)}
+      </li> 
+      }
+  }
+
+
   render(){
+
+    const orderIds = Object.keys(this.props.order)
+
+    const total = orderIds.reduce((prevTotal, key)=> {  
+      const fish = this.props.fishes[key]
+      const count = this.props.order[key]
+      // check first that the fish is still in the inventory AND that it's available
+      const isAvailable = fish && fish.status === 'available'
+      if(isAvailable){
+        return prevTotal + (count * fish.price)
+      }
+      return prevTotal;
+      // starting value of 0 as second paramter in reduce method
+    }, 0)
+    
     return (
-      <div>order</div>
+
+      <div className="order-wrap">
+        <h2>Order</h2>
+        <ul className="order">
+          {orderIds.map(this.renderOrder)}
+        </ul>
+        <div className="total">
+          Total: 
+          <strong> {formatPrice(total)} </strong>
+        </div>
+      </div>
 
     )
   }
